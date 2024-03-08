f2d4ffbd4ea0f3a5392e36646693531a73f6d2d6
Guillaume240192 <146423617+Guillaume240192@users.noreply.github.com>
2024-03-08 16:23:34 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,44 +1,6 @@
-
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-  }
-
-async function fetchData() {
-    try {
-        
-        // pokemon
-        var response = await fetch(`https://pokeapi.co/api/v2/pokemon/${getRandomInt(1025)}`);
-
-        if(!response.ok) {
-            throw new Error("Could not fetch resource");
-        }
-
-        const data = await response.json();
-        const pokemonSprite = data.sprites.front_default;
-        const pokemonName = data.name;
-        const inputElement = document.getElementById("pokemonName");
-        inputElement.style.display = "block";
-        document.getElementById("pokemonName").value = pokemonName;
-
-        const imgElement = document.getElementById("pokemonSprite")
-        //console.log(imgElement);
-        imgElement.src = pokemonSprite;
-        imgElement.style.display ="block";
-
-
-        // Chuck norris
-        var response2 = await fetch(`https://api.chucknorris.io/jokes/random`);
-        const data2 = await response2.json();
-        const joke = data2.value;
-
-        const pElement = document.getElementById("chuckQuote");
-        pElement.innerHTML = 'Once Chuck Norris said : <br>"'+ joke +'"';
-
-
-    }
-    catch(error) {
-        console.error(error);
-    }
+const body = document.querySelector("body");
+const toggle = document.getElementById("toggle");
+toggle.onclick = function() {
+    toggle.classList.toggle("active");
+    body.classList.toggle("active");
 }
-fetchData();
-let myInterval = setInterval(fetchData, 5000);
